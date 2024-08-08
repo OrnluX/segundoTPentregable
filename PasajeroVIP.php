d86eb6dea4f8c3208b0b1d630a2befe39818fad8
OrnluX <ivantarquini91@gmail.com>
2024-08-08 17:49:15 -0300

    Implementación básica de la clase PasajeroVIP

---
+++ PasajeroVIP.php
@@ -0,0 +1,38 @@
+<?php
+    include_once 'Pasajeros.php';
+    class PasajeroVIP extends Pasajeros {
+        private $nroViajeroFrecuente;
+        private $cantMillas;
+
+        public function __construct(string $nombre, int $nroTicket, int $nroAsiento, int $nroViajeroFrecuente, float $cantMillas)
+        {
+            parent::__construct($nombre, $nroTicket, $nroAsiento);
+            $this->nroViajeroFrecuente = $nroViajeroFrecuente;
+            $this->cantMillas = $cantMillas;
+        }
+
+        //GETTERS
+        public function getNroViajeroFrecuente() {
+            return $this->nroViajeroFrecuente;
+        }
+
+        public function getCantMillas() {
+            return $this->cantMillas;
+        }
+
+        //SETTERS
+        public function setNroViajeroFrecuente($nroViajeroFrecuente) {
+            $this->nroViajeroFrecuente = $nroViajeroFrecuente;
+        }
+
+        public function setCantMillas($cantMillas) {
+            $this->cantMillas = $cantMillas;
+        }
+
+        public function __toString()
+        {
+            $cadena = parent:: __toString();
+            $cadena . "\n Número de viajero frecuente: " . $this->getNroViajeroFrecuente() . "\n Cantidad de millas: " . $this->getCantMillas(); 
+        }
+    }
+?>
--- Pasajeros.php
+++ Pasajeros.php
@@ -1,6 +1,6 @@
 <?php
     /**
-     * La empresa de transporte desea gestionar la información correspondiente a los pasajeros de los Viajes que pueden ser: Pasajeros estándares, Pasajeros VIP y Pasajeros con necesidades especiales. 
+        La empresa de transporte desea gestionar la información correspondiente a los pasajeros de los Viajes que pueden ser: Pasajeros estándares, Pasajeros VIP y Pasajeros con necesidades especiales. 
 
         La clase Pasajero tiene como atributos el nombre, el número de asiento y el número de ticket del pasaje del viaje. La clase "PasajeroVIP" tiene como atributos adicionales el número de viajero frecuente y cantidad de millas de pasajero. La clase Pasajeros con necesidades especiales se refiere a pasajeros que pueden requerir servicios especiales como sillas de ruedas, asistencia para el embarque o desembarque, o comidas especiales para personas con alergias o restricciones alimentarias.  Implementar el método darPorcentajeIncremento() que retorne el porcentaje que debe aplicarse como incremento según las características del pasajero. Para un pasajero VIP se incrementa el importe un 35% y si la cantidad de millas acumuladas supera a las 300 millas se realiza un incremento del 30%. Si el pasajero tiene necesidades especiales y requiere silla de ruedas, asistencia y comida especial entonces el pasaje tiene un incremento del 30%; si solo requiere uno de los servicios prestados entonces el incremento es del 15%. Por último, para los pasajeros comunes el porcentaje de incremento es del 10 %.
 
